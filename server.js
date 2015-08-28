41583bebc2af6c5837d73b464f2fb9040455c19f
Steven Follis <steven_follis@live.com>
2015-08-28 14:48:01 -0400

    Removed web behavior

--- server.js
+++ server.js
@@ -1,13 +1,2 @@
 require('coffee-script/register');
-
-// Setup simple web response
-var http = require('http');
-var server = http.createServer(function (request, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("This is Harrisonbot, becoming sentient and ready for action\n");
-});
-
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(80);
-
 module.exports = require('hubot/bin/hubot.coffee');
